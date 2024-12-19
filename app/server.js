e86e85558ee5570bbe0c1fb52df3d452a4ef5ba6
Mangayarkarasi.S <mangaisuresh2923@gmail.com>
2024-12-19 23:47:37 +0530

    Docker-compose and Server.js Updated

--- app/server.js
+++ app/server.js
@@ -5,76 +5,73 @@ let MongoClient = require('mongodb').MongoClient;
 let bodyParser = require('body-parser');
 let app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/profile-picture', function (req, res) {
+app.get('/profile-picture', (req, res) => {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-// use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+// Docker MongoDB connection string
+let mongoUrlDocker = "mongodb://admin:password@mongodb:27017";
 
-// use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
-
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-// "user-account" in demo with docker. "my-db" in demo with docker-compose
+// Database and collection details
 let databaseName = "my-db";
+let collectionName = "users";
 
-app.post('/update-profile', function (req, res) {
-  let userObj = req.body;
+// Endpoint to update profile
+app.post('/update-profile', async (req, res) => {
+  try {
+    let userObj = req.body;
+    console.log("Updating profile:", userObj);
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
+    const client = await MongoClient.connect(mongoUrlDocker, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to MongoDB");
 
-    let db = client.db(databaseName);
+    const db = client.db(databaseName);
     userObj['userid'] = 1;
 
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
+    const myquery = { userid: 1 };
+    const newvalues = { $set: userObj };
+    console.log("Updating user with query:", myquery, "values:", newvalues);
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
-    });
+    const result = await db.collection(collectionName).updateOne(myquery, newvalues, { upsert: true });
+    console.log("Update result:", result);
 
-  });
-  // Send response
-  res.send(userObj);
+    await client.close();
+    res.send(userObj);
+  } catch (err) {
+    console.error("Error updating profile:", err);
+    res.status(500).send({ error: "Failed to update profile" });
+  }
 });
 
-app.get('/get-profile', function (req, res) {
-  let response = {};
-  // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
+// Endpoint to get profile
+app.get('/get-profile', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(mongoUrlDocker, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to MongoDB");
 
-    let db = client.db(databaseName);
+    const db = client.db(databaseName);
+    const myquery = { userid: 1 };
 
-    let myquery = { userid: 1 };
+    const result = await db.collection(collectionName).findOne(myquery);
+    console.log("Fetched profile:", result);
 
-    db.collection("users").findOne(myquery, function (err, result) {
-      if (err) throw err;
-      response = result;
-      client.close();
-
-      // Send response
-      res.send(response ? response : {});
-    });
-  });
+    await client.close();
+    res.send(result || {});
+  } catch (err) {
+    console.error("Error fetching profile:", err);
+    res.status(500).send({ error: "Failed to fetch profile" });
+  }
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
 });
